8fb57ab35d7036c7cf8df0129f120021d8e2b5f5
Quang-Linh LE <linktohack@gmail.com>
2019-08-16 22:44:14 +0200

    Draw dummy screen

---
+++ kilo.c
@@ -0,0 +1,97 @@
+// -- include
+
+#include <ctype.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+
+// -- defines
+
+#define CTRL_KEY(k) ((k) & 0x1f)
+
+// -- data
+
+struct termios orig_termios;
+
+// -- terminal
+
+void die(const char *s) {
+  write(STDOUT_FILENO, "\x1b[2J", 4); // clear screen
+  write(STDOUT_FILENO, "\x1b[H", 3);  // jum `back to top
+
+  perror(s);
+  exit(1);
+}
+
+void disableRawMode() {
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) die("tcsetattr");
+}
+
+void enableRawMode() {
+  if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
+  atexit(disableRawMode);
+
+  struct termios raw = orig_termios;
+  raw.c_iflag &= ~(ICRNL | IXON | BRKINT | INPCK | ISTRIP);
+  raw.c_oflag &= ~(OPOST);
+  raw.c_cflag |= (CS8);
+  raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
+  raw.c_cc[VMIN] = 0;
+  raw.c_cc[VTIME] = 1;
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
+}
+
+char editorReadKey() {
+  int nread;
+  char c;
+  
+  while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+    if (nread == -1 && errno != EAGAIN) die("read");
+  }
+  return c;
+}
+
+// -- output
+
+void editorDrawRows() {
+  int y;
+  for (y = 0; y < 24; y++) {
+    write(STDOUT_FILENO, "~\r\n", 3);
+  }
+}
+
+void editorRefreshScreen() {
+  write(STDOUT_FILENO, "\x1b[2J", 4); // clear screen
+  write(STDOUT_FILENO, "\x1b[H", 3);  // jump back to top
+  editorDrawRows();
+  write(STDOUT_FILENO, "\x1b[H", 3);  // jump back to top
+}
+
+// -- input
+
+void editorProcessKeypress() {
+  char c = editorReadKey();
+
+  switch(c) {
+  case CTRL_KEY('q'):
+    write(STDOUT_FILENO, "\x1b[2J", 4); // clear screen
+    write(STDOUT_FILENO, "\x1b[H", 3);  // jum `back to top
+    exit(0);
+    break;
+  }
+}
+
+// -- init
+
+int main() {
+  enableRawMode();
+  
+  while (1) {
+    editorRefreshScreen();
+    editorProcessKeypress();
+  };
+
+  return 0;
+}
