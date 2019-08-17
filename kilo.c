761be1de6addfe41b87c9baa53eb57091010b850
Quang-Linh LE <linktohack@gmail.com>
2019-08-17 17:12:58 +0200

    Incsearch

--- kilo.c
+++ kilo.c
@@ -69,7 +69,7 @@ struct editorConfig E;
 
 void editorSetStatusMessage(const char *fmt, ...);
 void editorRefreshScreen();
-char *editorPrompt(char *prompt);
+char *editorPrompt(char *prompt, void (*callback)(char *, int));
 
 // -- terminal
 
@@ -384,7 +384,7 @@ void editorOpen(char *filename) {
 
 void editorSave() {
   if (E.filename == NULL) {
-    E.filename = editorPrompt("Save as: %s");
+    E.filename = editorPrompt("Save as: %s", NULL);
     if (E.filename == NULL) {
       editorSetStatusMessage("Save aborted");
       return;
@@ -413,21 +413,61 @@ void editorSave() {
 
 // -- find
 
-void editorFind() {
-  char *query = editorPrompt("Search: %s (ESC to cancel)");
-  if (query == NULL) return;
+void editorFindCallback(char *query, int key) {
+  static int last_match = -1;
+  static int direction = 1;
+
+  if (key == '\r' || key == '\x1b') {
+    last_match = -1;
+    direction = 1;
+    return;
+  } else if (key == ARROW_LEFT || key == ARROW_UP) {
+    direction = 1;
+  } else if (key == ARROW_RIGHT || key == ARROW_DOWN) {
+    direction = -1;
+  } else {
+    last_match = -1;
+    direction = 1;
+  }
+
+  if (last_match == -1) direction = 1;
+  int current = last_match;
 
   for (int i = 0; i < E.numrows; i++) {
-    erow *row = &E.row[i];
+    current += direction; // up or down
+    if (current == -1) {
+      current = E.numrows - 1;
+    }
+    if (current == E.numrows) {
+      current = 0;
+    }
+
+    erow *row = &E.row[current];
     char *match = strstr(row->render, query);
     if (match) {
-      E.cy = i;
+      last_match = current;
+      E.cy = current;
       E.cx = editorRowRxToCx(row, match - row->render);
       E.rowoff = E.numrows;
     }
   }
+}
 
-  free(query);
+void editorFind() {
+  int saved_cx = E.cx;
+  int saved_cy = E.cy;
+  int saved_coloff = E.coloff;
+  int saved_rowoff = E.rowoff;
+
+  char *query = editorPrompt("Search: %s (Use ESC/Arrows/Enter)", editorFindCallback);
+  if (query) {
+    free(query);
+  } else {
+    E.cx = saved_cx;
+    E.cy = saved_cy;
+    E.coloff = saved_coloff;
+    E.rowoff = saved_rowoff;
+  }
 }
 
 // -- append buffer
@@ -572,9 +612,9 @@ void editorSetStatusMessage(const char *fmt, ...) {
 
 // -- input
 
-char *editorPrompt(char *prompt) {
+char *editorPrompt(char *prompt, void(*callback)(char *, int)) {
   size_t bufsize = 128;
-  char *buf = malloc(bufsize); // when will it be free?
+  char *buf = malloc(bufsize); // when will it be free? up to the caller to do it
 
   size_t buflen = 0;
   buf[0] = '\0';
@@ -588,11 +628,13 @@ char *editorPrompt(char *prompt) {
       if (buflen != 0) buf[buflen--] = '\0';
     } else if (c == '\x1b') {
       editorSetStatusMessage("");
+      if (callback) callback(buf, c);
       free(buf);
       return NULL;
     } else if (c == '\r') {
       if (buflen != 0) {
 	editorSetStatusMessage("");
+	if (callback) callback(buf, c);
 	return buf;
       }
     } else if (!iscntrl(c) && c < 128){
@@ -603,6 +645,7 @@ char *editorPrompt(char *prompt) {
       buf[buflen++] = c;
       buf[buflen] = '\0';
     }
+    if (callback) callback(buf, c);
   }
 }
 
