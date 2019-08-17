6edffe11b6f53ff31b7e7f612fffbf2917055460
Quang-Linh LE <linktohack@gmail.com>
2019-08-17 15:51:03 +0200

    With search

--- kilo.c
+++ kilo.c
@@ -195,6 +195,21 @@ int editorRowCxToRx(erow *row, int cx) {
   return rx;
 }
 
+int editorRowRxToCx(erow *row, int rx) {
+  int cur_rx = 0;
+  int cx;
+
+  for (cx = 0; cx < row->size; cx++) {
+    if (row->chars[cx] == '\t')
+      cur_rx += (KILO_TAB_STOP - 1) - (cur_rx % KILO_TAB_STOP);
+    cur_rx++;
+
+    if (cur_rx > rx) return cx;
+  }
+  return cx;
+}
+
+
 void editorUpdateRow(erow *row) {
   int tabs = 0;
   int j;
@@ -394,6 +409,25 @@ void editorSave() {
   }
   free(buf);
   editorSetStatusMessage("Can't save file! I/O error: %s", strerror(errno));
+}
+
+// -- find
+
+void editorFind() {
+  char *query = editorPrompt("Search: %s (ESC to cancel)");
+  if (query == NULL) return;
+
+  for (int i = 0; i < E.numrows; i++) {
+    erow *row = &E.row[i];
+    char *match = strstr(row->render, query);
+    if (match) {
+      E.cy = i;
+      E.cx = editorRowRxToCx(row, match - row->render);
+      E.rowoff = E.numrows;
+    }
+  }
+
+  free(query);
 }
 
 // -- append buffer
@@ -683,6 +717,10 @@ void editorProcessKeypress() {
     editorMoveCursor(c);
     break;
 
+  case CTRL_KEY('r'):
+    editorFind();
+    break;
+
   case CTRL_KEY('l'):
   case '\x1b':
     break;
@@ -721,7 +759,7 @@ int main(int argc, char *argv[]) {
     editorOpen(argv[1]);
   }
 
-  editorSetStatusMessage("HELP: Ctrl-Q = quit | Ctrl-S = save");
+  editorSetStatusMessage("HELP: Ctrl-Q = quit | Ctrl-S = save | Ctrl-R = find");
 
   while (1) {
     editorRefreshScreen();
